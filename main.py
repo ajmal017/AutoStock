6b856d03690e2662dec373b5cfb2049ea98d1224
Wesley Dashner <wdashner11@gmail.com>
2019-09-01 19:37:27 -0600

    got all necessary functions written

--- main.py
+++ main.py
@@ -1,29 +1,93 @@
 from shell import RobinhoodShell
+from enum import Enum
 
 robinhood_shell = RobinhoodShell()
 
 
+class Action(Enum):
+    BUY = 'buy'
+    SELL = 'sell'
+    HOLD = 'hold'
+
+
+def get_prices_file(symbol):
+    return 'data/' + symbol + '/prices.txt'
+
+
+def get_last_action_file(symbol):
+    return 'data/' + symbol + '/last-action.txt'
+
+
+def set_last_action(symbol, action):
+    file = open(get_last_action_file(symbol), 'w')
+    file.write(action)
+    file.close()
+
+
 def add_new_price(price, symbol):
-    desired_length = 5
-    file = open(symbol + '.txt', 'r')
-    prices = []
-    for line in file:
-        prices.append(float(line))
-    if len(prices) >= desired_length:
-        prices.pop(0)
-    prices.append(float(price))
-    file.close()
-
-    file = open(symbol + '.txt', 'w')
-    for price in prices:
-        file.write(str(price) + '\n')
+    file = open(get_prices_file(symbol), 'a')
+    file.write('\n' + str(price))
     file.close()
 
 
-def get_price(symbol):
+def get_current_price(symbol):
     instrument_url = robinhood_shell.get_instrument(symbol)['url']
     data = robinhood_shell.trader.get_stock_marketdata([instrument_url])
     return float(data[0]['last_trade_price'])
 
 
-add_new_price(get_price('TSLA'), 'TSLA')
+def get_average(symbol, price_count):
+    file = open(get_prices_file(symbol), 'r')
+    all_prices = []
+    for line in file:
+        all_prices.append(float(line))
+    file.close()
+    prices = all_prices[-price_count:]
+    return sum(prices) / len(prices)
+
+
+def get_last_action(symbol):
+    file = open(get_last_action_file(symbol), 'r')
+    action = None
+    for line in file:
+        action = line
+    file.close()
+    return action
+
+
+def get_action(symbol, price_count):
+    price = get_current_price(symbol)
+    average = get_average(symbol, price_count)
+    last_action = get_last_action(symbol)
+    if price > average and last_action == 'sell':
+        return Action.BUY
+    elif price < average and last_action == 'buy':
+        return Action.SELL
+    else:
+        return Action.HOLD
+
+
+def buy(symbol, quantity):
+    instrument = robinhood_shell.get_instrument(symbol)
+    robinhood_shell.trader.place_buy_order(instrument, quantity)
+
+
+def sell(symbol, quantity):
+    instrument = robinhood_shell.get_instrument(symbol)
+    robinhood_shell.trader.place_sell_order(instrument, quantity)
+
+
+def is_market_open():
+    return False
+
+
+def main(symbol, quantity, price_count):
+    action = get_action(symbol)
+    if action == Action.BUY:
+        buy(symbol, quantity)
+    elif action == Action.SELL:
+        sell(symbol, quantity)
+
+
+if __name__ == '__main__':
+    main('GRPN', 1, 5)
