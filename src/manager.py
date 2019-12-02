96aa574cc584a270a8fb700e5c0b0f82b2d7ecbb
Wesley Dashner <wdashner11@gmail.com>
2019-12-01 20:40:29 -0700

    made manager print out what it does

--- src/manager.py
+++ src/manager.py
@@ -13,5 +13,11 @@ class Manager:
         move = strategy.get_current_move(symbol)
         if move == Move.BUY:
             self.buy(symbol, shares_count)
+            print('bought {shares_count} shares of {symbol}'.format(shares_count=shares_count, symbol=symbol))
         elif move == Move.SELL:
             self.sell(symbol, shares_count)
+            print('sold {shares_count} shares of {symbol}'.format(shares_count=shares_count, symbol=symbol))
+        elif move == Move.HOLD:
+            print('held {symbol}'.format(symbol=symbol))
+        elif not move:
+            print('market is closed')
