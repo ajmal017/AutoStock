4b916fecc49d5fe9363f1425df914249b5091232
Wesley Dashner <wdashner11@gmail.com>
2019-12-03 16:15:41 -0700

    best intervals now write to a file with n bests

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ def main():
     strategy = MeanReversion(26, 2)
     # print(strategy.get_mock_percentage('ACB', strategy.get_datetime_object('2019-01-01'), datetime.now()))
     # print(strategy.get_hold_percentage('ACB', strategy.get_datetime_object('2019-01-01'), datetime.now()))
-    print(strategy.get_best_intervals('VOO', strategy.get_datetime_object('2015-01-01'), datetime.now()))
+    strategy.do_best_intervals('ACB', strategy.get_datetime_object('2019-01-01'), datetime.now(), 'best_intervals/ACB')
+
 
 main()
--- src/strategies/mean_reversion.py
+++ src/strategies/mean_reversion.py
@@ -5,15 +5,17 @@ from move import Move
 
 class MeanReversion(Strategy):
 
-    def __init__(self, long_term_days_count, short_term_days_count):
-        self.long_term_days_count = long_term_days_count
-        self.short_term_days_count = short_term_days_count
-        self.days_back_length = long_term_days_count
+    def __init__(self, long_term_interval, short_term_interval):
+        self.long_term_interval = long_term_interval
+        self.short_term_interval = short_term_interval
+        self.days_back_length = long_term_interval
+        self.best_intervals = []  # made of tuples (long_term_interval, short_term_interval, percent)
+        self.best_intervals_length = 10
 
     def get_move(self, prices, last_move, index=-1):
         which_price = 'Adj Close'
-        long_term_average = mean([price[which_price] for price in prices[index - self.long_term_days_count:index - 1]])
-        short_term_average = mean([price[which_price] for price in prices[index - self.short_term_days_count:index - 1]])
+        long_term_average = mean([price[which_price] for price in prices[index - self.long_term_interval:index - 1]])
+        short_term_average = mean([price[which_price] for price in prices[index - self.short_term_interval:index - 1]])
         if short_term_average < long_term_average and last_move == Move.SELL:
             return Move.BUY
         elif short_term_average > long_term_average and last_move == Move.BUY:
@@ -21,20 +23,32 @@ class MeanReversion(Strategy):
         else:
             return Move.HOLD
 
-    def get_best_intervals(self, symbol, start_date, end_date, long_term_minimum=25, long_term_maximum=200, short_term_minimum=2, short_term_maximum=50):
+    def do_best_intervals(self, symbol, start_date, end_date, directory, long_term_minimum=25, long_term_maximum=200, short_term_minimum=2, short_term_maximum=50):
         self.days_back_length = long_term_maximum  # to get prices far back enough
         prices = self.get_prices(symbol, start_date, end_date)
-        best_intervals = None
-        best_percent = None
         for long_term in range(long_term_minimum, long_term_maximum + 1):
             print(str(long_term - long_term_minimum + 1) + '/' + str(long_term_maximum - long_term_minimum + 1))
             short_term_maximum = short_term_maximum if short_term_maximum < long_term else long_term - 1
             for short_term in range(short_term_minimum, short_term_maximum + 1):
-                self.long_term_days_count = long_term
-                self.short_term_days_count = short_term
+                self.long_term_interval = long_term
+                self.short_term_interval = short_term
                 percent = self.get_mock_percentage(0, 0, 0, prices=prices[abs(long_term_maximum - long_term):])
-                if best_percent is None or percent > best_percent:
-                    best_percent = percent
-                    best_intervals = (long_term, short_term)
-        print(best_percent)
-        return best_intervals
+                self.add_to_best(long_term, short_term, percent)
+        self.write_to_file(directory, start_date, end_date)
+
+    def add_to_best(self, long_term, short_term, percent):
+        if len(self.best_intervals) < self.best_intervals_length:
+            self.best_intervals.append((long_term, short_term, percent))
+            self.best_intervals.sort(key=lambda x: x[-1], reverse=True)
+        elif percent > self.best_intervals[-1][-1]:
+            self.best_intervals.pop(-1)
+            self.best_intervals.append((long_term, short_term, percent))
+            self.best_intervals.sort(key=lambda x: x[-1], reverse=True)
+
+    def write_to_file(self, directory, start_date, end_date):
+        file = open(directory + '/' + str(start_date.date()) + '--' + str(end_date.date()) + '.txt', 'w+')
+        file.write(str(start_date.date()) + ' - ' + str(end_date.date()) + '\n\n')
+        file.write('long,short yields percent%\n\n')
+        for intervals in self.best_intervals:
+            file.write(str(intervals[0]) + ',' + str(intervals[1]) + ' yields ' + str(intervals[2]) + '%\n')
+        file.close()
